d1b4885105c96df5b89c709f10266d6d30906510
shaff600 <shafyl@live.co.uk>
2021-03-22 17:11:02 +0000

    inital layout of js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// querySelector, difficultySelect, categorySelect global variables
+//current score variable - empty array
+//available questions -empty array
+
+
+
+//API request
+
+
+//collect questions from api 
+
+//submit game options
+
+//start game
+
+
+//display correct/incorrect answer
+
+
+//display next question
+
+
+//end game
+
+
+
+//increment correct answer
+//increment incorrect answer
+//progress bar
