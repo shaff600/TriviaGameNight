76bbeba78726f80d277ddea45f9873f978e776d1
shaff600 <shafyl@live.co.uk>
2021-03-22 21:59:37 +0000

    implmented submit eventlistner to hide/show elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,41 @@
 // querySelector, difficultySelect, categorySelect global variables
 //current score variable - empty array
 //available questions -empty array
+const quizSelectOpt = document.getElementById("gameOptions");
+const categoryOpt = document.getElementById("categorySelect");
+const difficultyOpt = document.getElementById("difficultySelect");
+const questionAmount = document.getElementById("quantityQuestion");
+const submitQuiz = document.getElementById("submitOptions");
 
 
+
+
+// function hideMenu() {
+//   if (submitQuiz.style.display === "none") {
+//     submitQuiz.style.display = "block";
+//   } else {
+//     submitQuiz.style.display = "none";
+//   }
+// }
+
+// submitQuiz.addEventListener('click', () =>{
+
+//     if(quizSelectOpt.style.display === 'none'){
+//         quizSelectOpt.style.display = 'block';
+//     }else{
+//         quizSelectOpt.style.display === 'none'
+//     }
+// })
+
+submitQuiz.addEventListener("click", function(){
+  quizSelectOpt.classList.add("d-none");
+  game.classList.remove("d-none")
+})
+
+
+// function myFunction() {
+//   quizSelectOpt.classList.add("d-none");
+// }
 
 //API request
 
