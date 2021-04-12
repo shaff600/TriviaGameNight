d3800e26cdc9f4ac7097d5e829b0c1e0cb1ec9e0
shaff600 <shafyl@live.co.uk>
2021-04-12 17:39:43 +0000

    fetch new question implmentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,26 @@
-// querySelector, difficultySelect, categorySelect global variables
-//current score variable - empty array
-//available questions -empty array
 const quizSelectOpt = document.getElementById("gameOptions");
 const categoryOpt = document.getElementById("category");
 const difficultyOpt = document.getElementById("difficultySelect");
 const questionAmount = document.getElementById("quantityQuestion");
 const submitQuiz = document.getElementById("submitOptions");
 const remove = document.getElementById("remove");
+const options = document.querySelectorAll(".answer-options"); 
+const question = document.querySelector("#question"); 
 let api_link = "https://opentdb.com/";
-let quantity
-let difficulty
-let gameOption
-let questions = []
-let formattedQuestion
-let questionCounter
-let availableQuestions = []
+let quantity;
+let difficulty;
+let gameOption;
+let questions = [];
+let formattedQuestion;
+let availableQuestions = [];
+let count = 0;
+let loadedQuestions;
 
+options.forEach(function (option) {
+  option.addEventListener("click", checkAnswer);
+});
 
-
-//SUBMIT GAME OPTIONS
-submitQuiz.addEventListener("click", function(){
+submitQuiz.addEventListener("click", function () {
   quizSelectOpt.classList.add("d-none");
   remove.classList.add("d-none");
   game.classList.remove("d-none");
@@ -27,94 +28,48 @@ submitQuiz.addEventListener("click", function(){
   gameOption = categoryOpt.value;
   difficulty = difficultyOpt.value;
   quantity = questionAmount.value;
-  getQuestions(gameOption, difficulty, quantity) // this calls the API at the end of the submitQuiz click event function
-})
-//API REQUEST
-function getQuestions(gameOption, difficulty, quantity){
-fetch(`${api_link}api.php?amount=${quantity}&category=${gameOption}&difficulty=${difficulty}&type=multiple`)
-.then(response => response.json())
-.then(loadedQuestion => {
-  console.log('**', loadedQuestion)
-  questions = loadedQuestion.results.map(loadedQuestion =>{
-  formattedQuestion = {
- question: loadedQuestion.question
-};
+  getQuestions(gameOption, difficulty, quantity);
+});
+function getQuestions(gameOption, difficulty, quantity) {
+  fetch(
+    `${api_link}api.php?amount=${quantity}&category=${gameOption}&difficulty=${difficulty}&type=multiple`
+  )
+    .then(response => response.json())
+    .then(loadedQuestion => {
+      questions = loadedQuestion.results.map(loadedQuestion => {
+        formattedQuestion = {
+          question: loadedQuestion.question,
+        };
+      });
 
-})
+      loadedQuestions = loadedQuestion.results;
+      console.log(loadedQuestions);
 
-displayQuestions(loadedQuestion);
-// .catch(err => {
-//   console.error(err)
-//   }) 
-})
-
-
+      displayNextQuestion();
+    });
 }
+function displayNextQuestion() {
+  // console.log("error here", loadedQuestions);
+  // const correctAnswer = loadedQuestions[count].correct_answer;
+  let answerSelection = [
+    ...loadedQuestions[count].incorrect_answers,
+    loadedQuestions[count].correct_answer,
+  ];
 
+  let shuffledAnswers = shuffle(answerSelection);
 
-// //FORMATTED QUESTIONS
-// function formatQuestion(getQuestions, questions){
-// const answerChoices = [... loadedQuestion.incorrect_answers]
-// formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-// answerChoices.splice(formattedQuestion.answer -1,0,
-//     loadedQuestion.correct_answer);
-//     answerChoices.forEach((choice, index) => {
-//     formattedQuestion["choice" + (index+1)] = choice;})
-//     // console.log(formattedQuestion)
-//     displayQuestions()
-// }  
-//DISPLAY QUESTIONS
-function displayQuestions(loadedQuestion){
-
-console.log(loadedQuestion)
-console.log(loadedQuestion.results[0].correct_answer)
-      let answerSelection = [...loadedQuestion.results[0].incorrect_answers, loadedQuestion.results[0].correct_answer] 
-     let  shuffledAnswers = shuffle(answerSelection) 
-      console.log(answerSelection)
-     document.querySelector("#question").innerHTML = `Question: ${loadedQuestion.results[0].question}`;
-     document.querySelector("#q1").innerHTML = answerSelection[0];
-     document.querySelector("#q2").innerHTML = answerSelection[1];
-     document.querySelector("#q3").innerHTML = answerSelection[2];
-     document.querySelector("#q4").innerHTML = answerSelection[3];
-    const userSelection = document.getElementsByClassName('answer-options')
-    var nodeList = document.querySelectorAll("p");
-    const childUserSelection = userSelection.childNodes
-    console.log(userSelection)
-   
-    for (let i = 0; i < userSelection.length; i++ ){
-      userSelection[i].addEventListener("click", function(){
-
-       console.log(this)
-       if(this.innerHTML == loadedQuestion.results[0].correct_answer){
-         alert("correct")
-         console.log(loadedQuestion)
-         document.querySelector("#question").innerHTML = `Question: ${loadedQuestion.results[0].question}`;
-         
-       }else{
-         alert("incorrect")
-       }
-       
-      }
-      
-      )
-    }
-    
-  }
-
-
-
-
-function loadQuestion(){
-  for(let i = 0; i > loadedQuestion.question; i++){
-    loadedQuestion[i].question
-  }
+  question.innerHTML = `Question: ${loadedQuestions[count].question}`;
+  options.forEach(function (option, index) {
+    option.innerHTML = shuffledAnswers[index];
+  });
 }
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -128,24 +83,25 @@ function shuffle(array) {
   return array;
 }
 
-// submitQuiz.addEventListener('click', getData)
-//collect questions from api 
 
-//submit game options
+function checkAnswer() {
+  if (this.innerHTML === loadedQuestions[count].correct_answer) {
+    alert("correct");
+  } else {
+    alert("incorrect");
+  }
+  getNextQuestion();
+}
+
+function getNextQuestion() {
+  count++;
+  console.log("count**", count);
+  if (count < loadedQuestions.length) {
+    displayNextQuestion(loadedQuestions);
+  } else {
+    alert("finished");
+    window.location = "../index.html/";
+  }
+}
 
 
-//start game
-
-//display correct/incorrect answer
-
-
-//display next question
-
-
-//end game
-
-
-
-//increment correct answer
-//increment incorrect answer
-//progress bar
